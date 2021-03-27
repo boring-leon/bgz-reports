bdb7448ef7ee8007c2a30b98de1dacb86bc58fe0
Leon Czerwinski <leon@leadguru.pl>
2021-03-27 13:06:51 +0100

    Fixed observer calculation

--- app/Observers/TransferObserver.php
+++ app/Observers/TransferObserver.php
@@ -10,20 +10,20 @@ class TransferObserver
         if($transfer->kwota > 0)
             $transfer->report->increment('salary', $transfer->kwota);
         else
-            $transfer->report->increment('expenses', $transfer->kwota);
+            $transfer->report->increment('expenses', abs($transfer->kwota));
     }   
 
     public function updated(Transfer $transfer){
         if($transfer->kwota > 0)
-            $transfer->report->increment('salary',$transfer->kwota);
+            $transfer->report->increment('salary', $transfer->kwota);
         else
-            $transfer->report->increment('expenses', $transfer->kwota);
+            $transfer->report->increment('expenses', abs($transfer->kwota));
     }
 
     public function deleted(Transfer $transfer){
         if($transfer->kwota > 0)
             $transfer->report->decrement('salary', $transfer->kwota);
         else
-            $transfer->report->decrement('expenses', $transfer->kwota);
+            $transfer->report->decrement('expenses', abs($transfer->kwota));
     }
 }
