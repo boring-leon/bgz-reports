8ccb0701cfad6334b84a6210784adb32400d4b37
Leon Czerwinski <leon@leadguru.pl>
2021-03-28 15:32:18 +0200

    Report model deals with calculating date diffs for home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Carbon\Carbon;
-
 class HomeController extends Controller
 {
     public function show(){
-        $avg_report_period_days = auth()->user()->reports->map(function($report){
-            return Carbon::createFromDate($report->start_date)->diffInDays(Carbon::createFromDate($report->end_date));
-        })->avg();
-
-        return view('home', compact('avg_report_period_days'));
+        return view('home');
     }
 }
--- app/Models/Report.php
+++ app/Models/Report.php
@@ -18,8 +18,12 @@ class Report extends Model
         return $this->salary - $this->expenses - $this->rent;
     }
 
-    public function getDurationAttribute(){
+    public function getHumanDurationAttribute(){
         return Carbon::parse($this->start_date)->diffForHumans(Carbon::parse($this->end_date), CarbonInterface::DIFF_ABSOLUTE);
+    }
+
+    public function getDaysDurationAttribute(){
+        return Carbon::createFromDate($this->start_date)->diffInDays(Carbon::createFromDate($this->end_date));
     }
 
     public function transfers(){
